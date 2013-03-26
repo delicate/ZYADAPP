08a97391b04e4e48f0d16f3343efab6d7a0c1742
delicate <delicate.tsao@gmail.com>
2013-03-26 11:26:29 +0800

    add client project folder

---
+++ client/励中助友/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  励中助友
+//
+//  Created by 曹礼鹏 on 13-3-25.
+//  Copyright (c) 2013年 zhuyouad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
